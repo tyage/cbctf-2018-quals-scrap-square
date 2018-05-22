d16caa6f8532e870f748c20eb1a35ad47fd79e5f
tyage <namatyage@gmail.com>
2018-05-22 22:00:08 +0900

    add layouts

--- app/src/index.js
+++ app/src/index.js
@@ -3,14 +3,13 @@ const path = require('path')
 
 const app = express()
 
-const staticDir = path.resolve(`${__dirname}/../static`)
-
 // set CSP to prevent XSS
 app.use((req, res, next) => {
   res.set('Content-Security-Policy', "default-src 'none'; script-src 'self'; style-src 'self'; img-src 'self'")
   next()
 })
 
+const staticDir = path.resolve(`${__dirname}/../static`)
 app.use('/static', express.static(staticDir))
 
 app.set('view engine', 'pug')
@@ -20,16 +19,17 @@ app.get('/', (req, res) => {
 })
 
 app.get('/new', (req, res) => {
-  res.sendFile(path.resolve(`${staticDir}/new.html`))
+  res.render('new')
 })
 app.post('/new', (req, res) => {
 })
 
 app.get('/scrap/:title', (req, res) => {
+  res.render('scrap')
 })
 
 app.get('/config', (req, res) => {
-  res.sendFile(path.resolve(`${staticDir}/config.html`))
+  res.render('config')
 })
 app.post('/config', (req, res) => {
 })

4140db068f4c44704d0f832a03c23bf01e9fc19a
tyage <namatyage@gmail.com>
2018-05-22 22:34:42 +0900

    add login

--- app/src/index.js
+++ app/src/index.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const session = require('express-session')
 const path = require('path')
 
 const app = express()
+
+app.use(session({
+  secret: 'XXXSECRETVALUE',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: true }
+}))
 
 // set CSP to prevent XSS
 app.use((req, res, next) => {
@@ -9,18 +17,36 @@ app.use((req, res, next) => {
   next()
 })
 
+// serve static files
+const staticBaseUri = '/static'
 const staticDir = path.resolve(`${__dirname}/../static`)
-app.use('/static', express.static(staticDir))
+app.use(staticBaseUri, express.static(staticDir))
 
 app.set('view engine', 'pug')
-
-app.get('/', (req, res) => {
-  res.render('index')
+app.use((req, res, next) => {
+  res.locals.staticBaseUri = staticBaseUri
+  res.locals.session = req.session
+  next()
 })
 
-app.get('/new', (req, res) => {
-  res.render('new')
+app.get('/login', (req, res) => res.render('login'))
+app.post('/login', (req, res) => {
 })
+app.get('/register', (req, res) => res.render('register'))
+app.post('/register', (req, res) => {
+})
+
+// require login below
+app.use((req, res, next) => {
+  if (!req.session.isLogin) {
+    return res.redirect('/login')
+  }
+  next()
+})
+
+app.get('/', (req, res) => res.render('index'))
+
+app.get('/new', (req, res) => res.render('new'))
 app.post('/new', (req, res) => {
 })
 
@@ -28,9 +54,7 @@ app.get('/scrap/:title', (req, res) => {
   res.render('scrap')
 })
 
-app.get('/config', (req, res) => {
-  res.render('config')
-})
+app.get('/config', (req, res) => res.render('config'))
 app.post('/config', (req, res) => {
 })
 

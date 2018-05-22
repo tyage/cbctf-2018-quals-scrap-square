b65cc821945c70c6de38b6d61da54f4249d0fde1
tyage <namatyage@gmail.com>
2018-05-22 21:47:58 +0900

    use pug template

--- app/src/index.js
+++ app/src/index.js
@@ -5,10 +5,33 @@ const app = express()
 
 const staticDir = path.resolve(`${__dirname}/../static`)
 
+// set CSP to prevent XSS
+app.use((req, res, next) => {
+  res.set('Content-Security-Policy', "default-src 'none'; script-src 'self'; style-src 'self'; img-src 'self'")
+  next()
+})
+
 app.use('/static', express.static(staticDir))
 
+app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(`${staticDir}/index.html`))
+  res.render('index')
+})
+
+app.get('/new', (req, res) => {
+  res.sendFile(path.resolve(`${staticDir}/new.html`))
+})
+app.post('/new', (req, res) => {
+})
+
+app.get('/scrap/:title', (req, res) => {
+})
+
+app.get('/config', (req, res) => {
+  res.sendFile(path.resolve(`${staticDir}/config.html`))
+})
+app.post('/config', (req, res) => {
 })
 
 app.listen(3000)

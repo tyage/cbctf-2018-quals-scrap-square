6970f8b7d80bc8ab17a3c58bf586cc2456aabdbc
tyage <namatyage@gmail.com>
2018-05-22 23:21:56 +0900

    save scrap in sqlite3

--- app/src/index.js
+++ app/src/index.js
@@ -1,14 +1,28 @@
 const express = require('express')
 const session = require('express-session')
 const path = require('path')
+const bodyParser = require('body-parser')
+const sqlite3 = require('sqlite3')
+
+const db = new sqlite3.Database('scrap.db')
+db.serialize(() => {
+  db.get('select count(*) from sqlite_master', (err, res) => {
+    if (res['count(*)'] == 0) {
+      db.run('create table users (id integer primary key, name text, password text)')
+      db.run('create table scraps (id integer primary key, user_id integer, title text, body text)')
+    }
+  })
+})
 
 const app = express()
 
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 app.use(session({
   secret: 'XXXSECRETVALUE',
   resave: false,
-  saveUninitialized: false,
-  cookie: { secure: true }
+  saveUninitialized: false
 }))
 
 // set CSP to prevent XSS
@@ -31,6 +45,13 @@ app.use((req, res, next) => {
 
 app.get('/login', (req, res) => res.render('login'))
 app.post('/login', (req, res) => {
+  // TODO: implement
+  req.session.user = {
+    id: 1,
+    name: req.body.name,
+    password: req.body.password
+  }
+  res.redirect('/')
 })
 app.get('/register', (req, res) => res.render('register'))
 app.post('/register', (req, res) => {
@@ -38,19 +59,32 @@ app.post('/register', (req, res) => {
 
 // require login below
 app.use((req, res, next) => {
-  if (!req.session.isLogin) {
+  if (!req.session.user) {
     return res.redirect('/login')
   }
   next()
 })
 
-app.get('/', (req, res) => res.render('index'))
+app.get('/', (req, res) => {
+  db.serialize(() => {
+    db.all('select * from scraps where user_id = ?', req.session.user.id, (err, rows) => {
+      res.render('index', { scraps: rows })
+    })
+  })
+})
 
 app.get('/new', (req, res) => res.render('new'))
 app.post('/new', (req, res) => {
+  db.serialize(() => {
+    db.run(
+      'insert into scraps (user_id, title, body) values (?, ?, ?)',
+      req.session.user.id, req.body.title, req.body.body
+    )
+    res.redirect('/')
+  })
 })
 
-app.get('/scrap/:title', (req, res) => {
+app.get('/scrap/:id', (req, res) => {
   res.render('scrap')
 })
 

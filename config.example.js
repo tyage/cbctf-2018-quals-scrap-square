04491a3445396d9e51191fd6666245ac7207750e
tyage <namatyage@gmail.com>
2018-07-16 00:40:51 +0900

    add example config

---
+++ config.example.js
@@ -0,0 +1,14 @@
+module.exports = {
+  serverUrl: 'http://localhost:3000/',
+  adminLogin: {
+    name: 'test',
+    password: 'test'
+  },
+  recaptcha: {
+    siteKey: '',
+    secretKey: ''
+  },
+  express: {
+    secret: ''
+  }
+}

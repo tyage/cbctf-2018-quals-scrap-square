9730e2b6884e9b604a5668af9682a7507607f355
tyage <namatyage@gmail.com>
2018-07-26 00:14:35 +0900

    use sqlite instead of sqlite3

--- app/src/index.js
+++ app/src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const session = require('express-session')
 const path = require('path')
 const bodyParser = require('body-parser')
-const sqlite3 = require('sqlite3')
+const sqlite = require('sqlite')
 const fs = require('fs')
 const Recaptcha = require('express-recaptcha').Recaptcha;
 const crypto = require('crypto')
@@ -16,29 +16,24 @@ const hashPassword = (password) => {
   return hash.digest('hex')
 }
 
-const db = new sqlite3.Database('scrap.db')
-db.serialize(() => {
-  db.get('select count(*) from sqlite_master', (err, res) => {
-    if (res['count(*)'] == 0) {
-      db.run('create table users (id integer primary key, uid text unique, name text unique, password text)')
-      db.run('create table reports (id integer primary key, uid string, url text, title text, body text)')
-      db.run(
-        'insert into users (uid, name, password) values (?, ?, ?)',
-        'admin', config.adminLogin.name, config.adminLogin.password
-      )
-    }
-  })
-})
+const initializeDB = async (db) => {
+  const result = await db.get('select count(*) from sqlite_master')
+  if (result['count(*)'] !== 0) {
+    return
+  }
 
-const generateUid = async () => {
-  const isUidUnique = (uid) => {
-    return new Promise((resolve, reject) => {
-      db.serialize(() => {
-        db.get('select * from users where uid = ?', uid, (err, user) => {
-          resolve(user === undefined)
-        })
-      })
-    })
+  await db.run('create table users (id integer primary key, uid text unique, name text unique, password text)')
+  await db.run('create table reports (id integer primary key, uid string, url text, title text, body text)')
+  await db.run(
+    'insert into users (uid, name, password) values (?, ?, ?)',
+    'admin', config.adminLogin.name, config.adminLogin.password
+  )
+}
+
+const generateUid = async (db) => {
+  const isUidUnique = async (uid) => {
+    const result = await db.get('select * from users where uid = ?', uid)
+    return result === undefined
   }
 
   let newUid
@@ -51,108 +46,6 @@ const generateUid = async () => {
   }
   return newUid
 }
-
-const app = express()
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(session({
-  secret: config.express.secret,
-  resave: false,
-  saveUninitialized: false
-}))
-
-// set CSP to prevent XSS
-app.use((req, res, next) => {
-  res.set('Content-Security-Policy', `
-    default-src 'none';
-    script-src 'self' https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js https://code.jquery.com/jquery-3.3.1.min.js http://www.google.com/recaptcha/api.js https://www.gstatic.com/recaptcha/;
-    style-src 'self' https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css;
-    img-src 'self';
-    frame-src https://www.google.com/recaptcha/;
-    connect-src 'self'
-  `.replace(/\n/g, ''))
-  next()
-})
-
-// serve static files
-const staticBaseUri = '/static'
-const staticDir = path.join(__dirname, '..', 'static')
-const rawStaticDir = path.join(staticDir, 'raw')
-app.use(staticBaseUri, express.static(staticDir))
-
-// see how to use pug http://expressjs.com/ja/guide/using-template-engines.html
-app.set('view engine', 'pug')
-app.use((req, res, next) => {
-  res.locals.staticBaseUri = staticBaseUri
-  res.locals.session = req.session
-  next()
-})
-
-app.get('/login', (req, res) => res.render('login'))
-app.post('/login', (req, res) => {
-  db.serialize(() => {
-    db.get(
-      'select id, name from users where name = ? AND password = ?',
-      req.body.name, hashPassword(req.body.password),
-      (err, user) => {
-        req.session.user = user
-        res.redirect('/')
-      }
-    )
-  })
-})
-app.get('/register', (req, res) => res.render('register'))
-app.post('/register', async (req, res) => {
-  // TODO: hash password
-  const errors = []
-  if (req.body.name.length > 80) {
-    errors.push('Username should be less than 80')
-  }
-  if (errors.length > 0) {
-    return res.render('register', { errors })
-  }
-
-  const uid = await generateUid()
-  db.run(
-    'insert into users (uid, name, password) values (?, ?, ?)',
-    uid, req.body.name, hashPassword(req.body.password),
-    function (err, user) {
-      if (err) {
-        errors.push(err)
-        return res.render('register', { errors })
-      }
-
-      const dirname = path.join(rawStaticDir, this.lastID.toString())
-      fs.mkdirSync(dirname)
-
-      res.redirect('/')
-    }
-  )
-})
-
-// require login below
-app.use((req, res, next) => {
-  if (!req.session.user || !req.session.user.id) {
-    return res.redirect('/login')
-  }
-  next()
-})
-
-app.get('/', (req, res) => {
-  const scrapsDir = path.join(rawStaticDir, req.session.user.uid)
-  fs.readdir(scrapsDir, (err, files) => {
-    if (err) {
-      files = []
-    }
-    res.render('index', { files })
-  })
-})
-
-app.get('/logout', (req, res) => {
-  req.session.user = null
-  res.redirect('/')
-})
 
 const isTitleValid = (title) => {
   const errors = []
@@ -178,91 +71,189 @@ const isBodyValid = (body) => {
   }
 }
 
-app.get('/new', (req, res) => res.render('new'))
-app.post('/new', (req, res) => {
-  // check body
-  const title = req.body.title.toString()
-  const body = req.body.body.toString()
-  const { errors: titleErrors } = isTitleValid(title)
-  const { errors: bodyErrors } = isBodyValid(body)
-  const errors = [ ...titleErrors, ...bodyErrors ]
-  if (errors.length > 0) {
-    return res.render('new', { errors })
-  }
+const startApp = async () => {
+  const db = await sqlite.open('./scrap.db')
+  initializeDB(db)
 
-  const filename = path.join(rawStaticDir, req.session.user.uid, title)
-  fs.writeFileSync(filename, body)
-  res.redirect(`/scraps/${req.session.user.uid}/${title}`)
-})
-app.post('/edit', (req, res) => {
-  // check body
-  const title = req.body.title.toString()
-  const body = req.body.body.toString()
-  const { errors: titleErrors } = isTitleValid(title)
-  const { errors: bodyErrors } = isBodyValid(body)
-  const errors = [ ...titleErrors, ...bodyErrors ]
-  if (errors.length > 0) {
-    return res.redirect(`/scraps/${req.session.user.uid}/${title}`)
-  }
+  const app = express()
 
-  const filename = path.join(rawStaticDir, req.session.user.uid, title)
-  fs.writeFileSync(filename, body)
-  res.redirect(`/scraps/${req.session.user.uid}/${title}`)
-})
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(session({
+    secret: config.express.secret,
+    resave: false,
+    saveUninitialized: false
+  }))
 
-app.post('/report', (req, res) => {
-  const doReport = () => {
-    if (!req.body.url.startsWith(config.serverUrl)) {
-      res.json({ success: false })
-      return
+  // set CSP to prevent XSS
+  app.use((req, res, next) => {
+    res.set('Content-Security-Policy', `
+      default-src 'none';
+      script-src 'self' https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js https://code.jquery.com/jquery-3.3.1.min.js http://www.google.com/recaptcha/api.js https://www.gstatic.com/recaptcha/;
+      style-src 'self' https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css;
+      img-src 'self';
+      frame-src https://www.google.com/recaptcha/;
+      connect-src 'self'
+    `.replace(/\n/g, ''))
+    next()
+  })
+
+  // serve static files
+  const staticBaseUri = '/static'
+  const staticDir = path.join(__dirname, '..', 'static')
+  const rawStaticDir = path.join(staticDir, 'raw')
+  app.use(staticBaseUri, express.static(staticDir))
+
+  // see how to use pug http://expressjs.com/ja/guide/using-template-engines.html
+  app.set('view engine', 'pug')
+  app.use((req, res, next) => {
+    res.locals.staticBaseUri = staticBaseUri
+    res.locals.session = req.session
+    next()
+  })
+
+  app.get('/login', (req, res) => res.render('login'))
+  app.post('/login', async (req, res) => {
+    const user = await db.get(
+      'select id, uid, name from users where name = ? AND password = ?',
+      req.body.name, hashPassword(req.body.password),
+    )
+
+    req.session.user = user
+    res.redirect('/')
+  })
+  app.get('/register', (req, res) => res.render('register'))
+  app.post('/register', async (req, res) => {
+    const errors = []
+    if (req.body.name.length > 80) {
+      errors.push('Username should be less than 80')
+    }
+    if (errors.length > 0) {
+      return res.render('register', { errors })
     }
 
-    db.run(
-      'insert into reports (uid, url, title, body) values (?, ?, ?, ?)',
-      req.body.to, req.body.url, req.body.title, req.body.body,
-      function (err, user) {
-        res.json({ success: true })
-      }
-    )
-  }
+    const uid = await generateUid(db)
+    try {
+      await db.run(
+        'insert into users (uid, name, password) values (?, ?, ?)',
+        uid, req.body.name, hashPassword(req.body.password)
+      )
 
-  // only admin can bypass recaptcha
-  if (req.session.user.uid === 'admin') {
-    doReport()
-  } else {
-    // check captcha
-    recaptcha.verify(req, (error, data) => {
-      if (error) {
+      // create user directory
+      const dirname = path.join(rawStaticDir, uid)
+      fs.mkdirSync(dirname)
+
+      res.redirect('/')
+    } catch (err) {
+      errors.push(err)
+      return res.render('register', { errors })
+    }
+  })
+
+  // require login for below processes
+  app.use((req, res, next) => {
+    if (!req.session.user || !req.session.user.id) {
+      return res.redirect('/login')
+    }
+    next()
+  })
+
+  app.get('/', (req, res) => {
+    const scrapsDir = path.join(rawStaticDir, req.session.user.uid)
+    fs.readdir(scrapsDir, (err, files) => {
+      if (err) {
+        files = []
+      }
+      res.render('index', { files })
+    })
+  })
+
+  app.get('/logout', (req, res) => {
+    req.session.user = null
+    res.redirect('/')
+  })
+
+  app.get('/new', (req, res) => res.render('new'))
+  app.post('/new', (req, res) => {
+    // check body
+    const title = req.body.title.toString()
+    const body = req.body.body.toString()
+    const { errors: titleErrors } = isTitleValid(title)
+    const { errors: bodyErrors } = isBodyValid(body)
+    const errors = [ ...titleErrors, ...bodyErrors ]
+    if (errors.length > 0) {
+      return res.render('new', { errors })
+    }
+
+    const filename = path.join(rawStaticDir, req.session.user.uid, title)
+    fs.writeFileSync(filename, body)
+    res.redirect(`/scraps/${req.session.user.uid}/${title}`)
+  })
+  app.post('/edit', (req, res) => {
+    // check body
+    const title = req.body.title.toString()
+    const body = req.body.body.toString()
+    const { errors: titleErrors } = isTitleValid(title)
+    const { errors: bodyErrors } = isBodyValid(body)
+    const errors = [ ...titleErrors, ...bodyErrors ]
+    if (errors.length > 0) {
+      return res.redirect(`/scraps/${req.session.user.uid}/${title}`)
+    }
+
+    const filename = path.join(rawStaticDir, req.session.user.uid, title)
+    fs.writeFileSync(filename, body)
+    res.redirect(`/scraps/${req.session.user.uid}/${title}`)
+  })
+
+  app.post('/report', async (req, res) => {
+    const doReport = async () => {
+      if (!req.body.url.startsWith(config.serverUrl)) {
         res.json({ success: false })
-      } else {
-        doReport()
+        return
       }
-    })
-  }
-})
-app.get('/reports', (req, res) => {
-  db.serialize(() => {
-    db.all(
-      'select url, title, body from reports where uid = ?',
-      req.session.user.uid,
-      (err, reports) => {
-        res.json({ reports })
-      }
-    )
-  })
-})
 
-app.get('/scraps/:uid/:title', (req, res) => {
-  // admin and owner can view scrap
-  db.serialize(() => {
-    db.get('select * from users where uid = ?', req.params.uid, (err, user) => {
-      res.render('scrap', {
-        user,
-        title: req.params.title,
-        captcha: recaptcha.render()
+      try {
+        await db.run(
+          'insert into reports (uid, url, title, body) values (?, ?, ?, ?)',
+          req.body.to, req.body.url, req.body.title, req.body.body
+        )
+      } catch (err) {
+        return res.json({ success: false })
+      }
+      res.json({ success: true })
+    }
+
+    // only admin can bypass recaptcha
+    if (req.session.user.uid === 'admin') {
+      await doReport()
+    } else {
+      // check captcha
+      recaptcha.verify(req, async (error, data) => {
+        if (error) {
+          res.json({ success: false })
+        } else {
+          await doReport()
+        }
       })
+    }
+  })
+  app.get('/reports', async (req, res) => {
+    const reports = await db.all(
+      'select url, title, body from reports where uid = ?',
+      req.session.user.uid
+    )
+    res.json({ reports })
+  })
+
+  app.get('/scraps/:uid/:title', async (req, res) => {
+    const user = await db.get('select * from users where uid = ?', req.params.uid)
+    res.render('scrap', {
+      user,
+      title: req.params.title,
+      captcha: recaptcha.render()
     })
   })
-})
 
-app.listen(3000)
+  app.listen(3000)
+}
+startApp()

ccd555733fcfe4bc7ccd6eb9b283151f856174e1
tyage <namatyage@gmail.com>
2018-07-27 00:03:26 +0900

    change name limit 256 in v1

--- app/src/index.js
+++ app/src/index.js
@@ -125,8 +125,8 @@ const startApp = async () => {
   app.get('/register', (req, res) => res.render('register'))
   app.post('/register', async (req, res) => {
     const errors = []
-    if (req.body.name.length > 80) {
-      errors.push('Username should be less than 80')
+    if (req.body.name.length > 256) {
+      errors.push('Username should be less than 256')
     }
     if (errors.length > 0) {
       return res.render('register', { errors })
